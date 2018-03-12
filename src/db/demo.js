5bd8b9175c9d4327e0011f082de2d5a919262938
Zoe <lizhou1021@qq.com>
2018-03-12 23:09:33 +0800

    haha

--- src/db/demo.js
+++ src/db/demo.js
@@ -1,16 +1,25 @@
-const mongo=require('mongodb');
+const MongoClient=require('mongodb').MongoClient;
 const assert=require('assert');
 
-const MongoClient=mongo.MongoClient; //start server
-const Urls="mongodb://localhost:27017/demo1";
+const Urls="mongodb://localhost:27017/cad";
 
 MongoClient.connect(Urls,function(err,db){
   assert.equal(null,err);
 
-  //插入数据
-  db.collection("user").insert({"name":"lz","age":27,"pwd":"123"},(err,result)=>{
+  // //插入数据
+  // db.collection("t1").insert({"name":"lz","age":27},(err,result)=>{
+  //   assert.equal(err,null);
+  //   console.log(result);
+  // })
+  console.log("connect cad");
+  let cad=db.db("cad");
+  cad.createCollection('user',(err,res)=>{
     assert.equal(err,null);
-    console.log(result);
-    db.close();
+    console.log("创建集合 user");
   })
+  // cad.collection('user').insert({name:"zoe",pwd:"123"},(err,res)=>{
+  //   assert.equal(err,null);
+  //   console.log("插入数据成功");
+  // })
+  db.close();
 })
